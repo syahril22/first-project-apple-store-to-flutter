39c11552ace0a2a4c85a0b26d638282942d36b74
Syahril Sobirin <syahrilsobirin248@gmail.com>
2023-08-24 16:30:38 +0700

    use grid view to display the product list

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppleStore());
+  runApp(const AppleStore());
 }
 
 class Product {
@@ -14,7 +14,7 @@ class Product {
 }
 
 class AppleStore extends StatefulWidget {
-  AppleStore({Key? key}) : super(key: key);
+  const AppleStore({Key? key}) : super(key: key);
 
   @override
   State<AppleStore> createState() => _AppleStoreState();
@@ -42,6 +42,10 @@ class _AppleStoreState extends State<AppleStore> {
     Product('iPhone 15 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-15.jpg'),
     Product('iPhone 13 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-14.jpg'),
     Product('iPhone 11 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-11.jpg'),
+    Product('iPhone 11 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-14.jpg'),
+    Product('iPhone 15 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-15.jpg'),
+    Product('iPhone 13 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-14.jpg'),
+    Product('iPhone 11 Pro Max', 'Rp 8.899.000', 'assets/images/iphone-11.jpg'),
   ];
 
   @override
@@ -56,19 +60,19 @@ class _AppleStoreState extends State<AppleStore> {
               title: const Text(
                 'iBox Official Store',
                 style: TextStyle(
-                  color: Colors.black,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
                   fontSize: 20,
                 ),
               ),
               centerTitle: true,
-              backgroundColor: Colors.amber,
+              backgroundColor: Colors.black,
               // Tab-Bar
               bottom: PreferredSize(
-                preferredSize: Size.fromHeight(90.0),
+                preferredSize: const Size.fromHeight(90.0),
                 child: TabBar(
                     // style label tab
-                    labelColor: Colors.black,
+                    labelColor: Colors.white,
                     // style indikator
                     indicator: const BoxDecoration(
                         border: Border(
@@ -80,94 +84,93 @@ class _AppleStoreState extends State<AppleStore> {
               ),
             ),
             body: TabBarView(children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 65.0, right: 30.0),
-                child: ListView.builder(
+              // first page to App start
+              GridView.builder(
+                  padding: const EdgeInsets.all(25),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, //jumlah kolom
+                    mainAxisSpacing: 15.0, //jarak vertikal
+                    crossAxisSpacing: 15.0, // jarak horizontal
+                    childAspectRatio: 0.7, // rasio items
+                  ),
                   itemCount: produk.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        const SizedBox(height: 35),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Column(
-                              children: [
-                                Image.asset(
-                                  produk[index].gambar,
-                                  height: 100,
-                                  width: 100,
-                                ),
-                                Text(
-                                  produk[index].nama,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  produk[index].price,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  'Add to cart: ${produk[index].jumlahCart}',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Row(
-                                  children: [
-                                    ElevatedButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          if (produk[index].jumlahCart > 0) {
-                                            produk[index].jumlahCart--;
-                                          }
-                                        });
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.amber,
-                                        padding: EdgeInsets.all(2.0),
-                                      ),
-                                      child: const Icon(
-                                        Icons.remove_shopping_cart,
-                                        color: Colors.black,
-                                        size: 20,
-                                      ),
-                                    ),
-                                    const SizedBox(width: 5),
-                                    ElevatedButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          produk[index].jumlahCart++;
-                                        });
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.amber,
-                                        padding: EdgeInsets.all(2.0),
-                                      ),
-                                      child: const Icon(
-                                        Icons.add_shopping_cart,
-                                        color: Color.fromARGB(255, 27, 26, 26),
-                                        size: 20,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            produk[index].gambar,
+                            height: 100,
+                            width: 100,
+                          ),
+                          Text(
+                            produk[index].nama,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
-                            const SizedBox(width: 50),
-                          ],
-                        ),
-                        const SizedBox(height: 25),
-                      ],
+                          ),
+                          Text(
+                            produk[index].price,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Add to cart: ${produk[index].jumlahCart}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          // button cart start
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (produk[index].jumlahCart > 0) {
+                                      produk[index].jumlahCart--;
+                                    }
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey,
+                                  padding: const EdgeInsets.all(2.0),
+                                ),
+                                child: const Icon(
+                                  Icons.remove_shopping_cart,
+                                  color: Colors.black,
+                                  size: 20,
+                                ),
+                              ),
+                              const SizedBox(width: 5),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    produk[index].jumlahCart++;
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey,
+                                  padding: const EdgeInsets.all(2.0),
+                                ),
+                                child: const Icon(
+                                  Icons.add_shopping_cart,
+                                  color: Colors.black,
+                                  size: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          // button cart end
+                        ],
+                      ),
                     );
-                  },
-                ),
-              ),
+                  }),
+              // first page to App end
               const Center(
                   child: Text(
                 'Mac Book Page',
